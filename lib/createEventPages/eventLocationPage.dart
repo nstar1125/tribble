dbc6357905a52b342057f79ee20a6e5f25fa8da4
LEEDH <tjdrhr02@naver.com>
2022-11-05 14:36:43 +0900

    LDH before login out

--- lib/createEventPages/eventLocationPage.dart
+++ lib/createEventPages/eventLocationPage.dart
@@ -1,4 +1,12 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
+import 'package:google_maps_webservice/places.dart';
+import 'package:google_api_headers/google_api_headers.dart';
+
+const kGoogleApiKey = "AIzaSyDJtGaKCd-UALmx7Qnoxb6LwKmUXZpk-78";
+final homeScaffoldKey = GlobalKey<ScaffoldState>();
 
 class EventLocationPage extends StatefulWidget {
   const EventLocationPage({Key? key}) : super(key: key);
@@ -7,10 +15,23 @@ class EventLocationPage extends StatefulWidget {
   State<EventLocationPage> createState() => _EventLocationPageState();
 }
 
+
 class _EventLocationPageState extends State<EventLocationPage> {
+
+  // related google map
+  static const CameraPosition _initialCameraPosition = CameraPosition(target: LatLng(37.42796, -122.08574), zoom: 14.0);
+  final Set<Marker> _markers = {};
+  late GoogleMapController _controller;
+
+  // related google place
+  final Mode _mode = Mode.overlay;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: homeScaffoldKey,
+      extendBodyBehindAppBar: true,
+      resizeToAvoidBottomInset : false,
       appBar: AppBar(
         iconTheme: IconThemeData(
             color: Colors.black87
@@ -19,7 +40,72 @@ class _EventLocationPageState extends State<EventLocationPage> {
         elevation: 0.0,
 
       ),
-      body: Text("hi"),
+      body: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: _initialCameraPosition,
+            markers: _markers,
+            onMapCreated: (GoogleMapController controller) {
+              _controller = controller;
+            },
+          ),
+          ElevatedButton(
+            child: Text("검색"),
+            onPressed: _handlePressButton,
+          )
+        ],
+      ),
     );
+  }
+
+  Future<void> _handlePressButton() async {
+    // show input autocomplete with selected mode
+    // then get the Prediction selected
+    Prediction? p = await PlacesAutocomplete.show(
+      context: context,
+      apiKey: kGoogleApiKey,
+      onError: onError,
+      mode: _mode,
+      language: "ko",
+      strictbounds: false,
+      types: [""],
+      decoration: InputDecoration(
+        hintText: 'Search',
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      components: [Component(Component.country, "kr")],
+    );
+
+    displayPrediction(p!, homeScaffoldKey.currentState);
+  }
+
+  void onError(PlacesAutocompleteResponse response) {
+  }
+
+  Future<void> displayPrediction(Prediction p, ScaffoldState? currentState) async {
+    GoogleMapsPlaces places = GoogleMapsPlaces(
+      apiKey: kGoogleApiKey,
+      apiHeaders: await const GoogleApiHeaders().getHeaders()
+    );
+
+    PlacesDetailsResponse detail = await places.getDetailsByPlaceId(p.placeId!);
+
+    final lat = detail.result.geometry!.location.lat;
+    final lng = detail.result.geometry!.location.lng;
+
+    _markers.clear();
+    _markers.add(Marker(markerId: const MarkerId("0"), position: LatLng(lat, lng), infoWindow: InfoWindow(title: detail.result.name)));
+
+    setState(() {
+
+    });
+
+    _controller.animateCamera(CameraUpdate.newLatLngZoom(LatLng(lat, lng), 14.0));
+
   }
 }
