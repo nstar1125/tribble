7ec2a6b2674c882e912cebcae27c9c894f0bacce
han-0315 <g12359@cau.ac.kr>
2022-11-15 03:48:01 +0900

    helper수정/login-helper연결/groups chatting구현

--- lib/guide/loginPages/signInPage.dart
+++ lib/guide/loginPages/signInPage.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tribble_guide/chatPages/chatDB/DatabaseService.dart';
+import 'package:tribble_guide/chatPages/helper/helper_function.dart';
+import 'package:tribble_guide/guide/loungePage.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -13,9 +18,9 @@ class _SignInPageState extends State<SignInPage> {
   final _authentication = FirebaseAuth.instance;
   String userEmail = '';
   String userPassword = '';
-  void _tryValidation(){
+  void _tryValidation() {
     final isValid = _formKey.currentState!.validate();
-    if(isValid){
+    if (isValid) {
       _formKey.currentState!.save();
     }
   }
@@ -25,16 +30,14 @@ class _SignInPageState extends State<SignInPage> {
     return Scaffold(
         appBar: AppBar(
           elevation: 0,
-          iconTheme: IconThemeData(
-              color: Colors.black87
-          ),
-          title: Text("로그인",
+          iconTheme: IconThemeData(color: Colors.black87),
+          title: Text(
+            "로그인",
             style: TextStyle(
                 color: Colors.black87,
                 fontFamily: "GmarketSansTTF",
                 fontSize: 20,
-                fontWeight: FontWeight.bold
-            ),
+                fontWeight: FontWeight.bold),
           ),
           backgroundColor: Colors.white,
           centerTitle: true,
@@ -47,108 +50,108 @@ class _SignInPageState extends State<SignInPage> {
           child: SingleChildScrollView(
             child: Center(
                 child: Padding(
-                  padding: const EdgeInsets.only(left:20, right:20),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      children: <Widget> [
-                        SizedBox(height: 20),
-                        TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            validator: ((value) {
-                              if(value!.isEmpty || !value.contains('@')){
-                                return "올바른 이메일 형식으로 입력하세요";
-                              }
-                              return null;
-                            }),
-                            onSaved: ((value) {
-                              userEmail = value!;
-                            }),
-                            onChanged: (value){
-                              userEmail = value;
-                            },
-                            decoration: InputDecoration(
-                              floatingLabelBehavior: FloatingLabelBehavior.always,
-                              labelText: "이메일",
-                              labelStyle: TextStyle(
-                                fontFamily: "GmarketSansTTF",
-                                fontSize: 16,
-                              ),
-                            )
-                        ),
-                        SizedBox(height: 20),
-                        TextFormField(
-                            obscureText: true,
-                            validator: ((value) {
-                              if(value!.isEmpty){
-                                return "패스워드를 입력하세요";
-                              }
-                              return null;
-                            }),
-                            onSaved: ((value) {
-                              userPassword = value!;
-                            }),
-                            onChanged: (value){
-                              userPassword = value;
-                            },
-                            decoration: InputDecoration(
-                              floatingLabelBehavior: FloatingLabelBehavior.always,
-                              labelText: "패스워드",
-                              labelStyle: TextStyle(
-                                fontFamily: "GmarketSansTTF",
-                                fontSize: 16,
-                              ),
-                            )
-                        ),
-                        SizedBox(height: 40),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              backgroundColor: Colors.lightBlueAccent,
-                              minimumSize: const Size.fromHeight(40)
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20),
+                    TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        validator: ((value) {
+                          if (value!.isEmpty || !value.contains('@')) {
+                            return "올바른 이메일 형식으로 입력하세요";
+                          }
+                          return null;
+                        }),
+                        onSaved: ((value) {
+                          userEmail = value!;
+                        }),
+                        onChanged: (value) {
+                          userEmail = value;
+                        },
+                        decoration: InputDecoration(
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          labelText: "이메일",
+                          labelStyle: TextStyle(
+                            fontFamily: "GmarketSansTTF",
+                            fontSize: 16,
                           ),
-                          child: Text("로그인",
-                            style: TextStyle(
-                              fontFamily: "GmarketSansTTF",
-                              fontSize: 14,
+                        )),
+                    SizedBox(height: 20),
+                    TextFormField(
+                        obscureText: true,
+                        validator: ((value) {
+                          if (value!.isEmpty) {
+                            return "패스워드를 입력하세요";
+                          }
+                          return null;
+                        }),
+                        onSaved: ((value) {
+                          userPassword = value!;
+                        }),
+                        onChanged: (value) {
+                          userPassword = value;
+                        },
+                        decoration: InputDecoration(
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          labelText: "패스워드",
+                          labelStyle: TextStyle(
+                            fontFamily: "GmarketSansTTF",
+                            fontSize: 16,
+                          ),
+                        )),
+                    SizedBox(height: 40),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          backgroundColor: Colors.lightBlueAccent,
+                          minimumSize: const Size.fromHeight(40)),
+                      child: Text(
+                        "로그인",
+                        style: TextStyle(
+                          fontFamily: "GmarketSansTTF",
+                          fontSize: 14,
+                        ),
+                      ),
+                      onPressed: () async {
+                        _tryValidation();
+                        try {
+                          final newUser =
+                              await _authentication.signInWithEmailAndPassword(
+                                  email: userEmail, password: userPassword);
+                          QuerySnapshot snapshot = await DatabaseService(
+                                  uid: FirebaseAuth.instance.currentUser!.uid)
+                              .gettingUserData(userEmail);
+                          // saving the values to our shared preferences
+                          await HelperFunctions.saveUserLoggedInStatus(true);
+                          await HelperFunctions.saveUserEmailSF(userEmail);
+                          await HelperFunctions.saveUserNameSF(
+                              snapshot.docs[0]['fullName']);
+                          if (newUser.user != null) {
+                            Navigator.of(context).pushNamed("/toLoungePage");
+                          }
+                        } catch (e) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(
+                              "잘못된 이메일이나 패스워드입니다",
+                              style: TextStyle(
+                                fontFamily: "GmarketSansTTF",
+                                fontSize: 14,
+                              ),
                             ),
-                          ),
-                          onPressed: () async{
-                            _tryValidation();
-                            try{
-                              final newUser = await _authentication.signInWithEmailAndPassword(
-                                  email: userEmail,
-                                  password: userPassword);
-                              if(newUser.user != null){
-                                Navigator.of(context).pushNamed("/toLoungePage");
-                              }
-                            }catch(e){
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content:
-                                    Text("잘못된 이메일이나 패스워드입니다",
-                                      style: TextStyle(
-                                        fontFamily: "GmarketSansTTF",
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                    backgroundColor: Colors.lightBlueAccent,
-                                  )
-                              );
-                            }
-                          },
-                        )
-                      ],
-                    ),
-                  ),
-                )
-            ),
+                            backgroundColor: Colors.lightBlueAccent,
+                          ));
+                        }
+                      },
+                    )
+                  ],
+                ),
+              ),
+            )),
           ),
-        )
-    );
+        ));
   }
 }
-
-
