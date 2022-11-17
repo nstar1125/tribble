f95bc022f34f1974afffd05ba0ef3d4457dbd151
LEEDH <tjdrhr02@naver.com>
2022-11-17 19:44:58 +0900

    recommend UI

--- lib/traveler/createPlanPages/showNomiPage.dart
+++ lib/traveler/createPlanPages/showNomiPage.dart
@@ -4,10 +4,6 @@ import 'package:multi_image_picker/multi_image_picker.dart';
 import 'package:tribble_guide/guide/createEventPages/event.dart';
 
 //자동 추천된 경로 리스트를 보여주는 페이지입니다 = 후보라는 뜻의 nominate에서 따옴
-// 1. 다른 사람들이 많이 선택하는 인기있는 경로
-// 2. 내가 선호하는 키워드만 존재하는 경로
-// 3. 참신한 경로
-// 4. 과거에 내가 선택했던 경로
 // 여러 경로를 후보로 주고 고를 수 있게 하기
 
 class ShowNomiPage extends StatefulWidget {
@@ -20,6 +16,7 @@ class ShowNomiPage extends StatefulWidget {
 class _ShowNomiPageState extends State<ShowNomiPage> {
   CollectionReference collectionRef = FirebaseFirestore.instance.collection('events');
   Event selectedEvent = Event.fromJson(initEvent);
+  List<Event> events = [];
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +26,7 @@ class _ShowNomiPageState extends State<ShowNomiPage> {
         iconTheme: IconThemeData(
           color: Colors.black87,
         ),
-        title: Text("Recommended Tour List",
+        title: Text("Recommendation",
           style: TextStyle(
               color: Colors.black87,
               fontFamily: "GmarketSansTTF",
@@ -39,7 +36,111 @@ class _ShowNomiPageState extends State<ShowNomiPage> {
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      body: Text(""),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("# Popular Tour in Now",
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontFamily: "GmarketSansTTF",
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold
+                  ),),
+              ],
+            ),
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: () async {
+
+                final eventInfo1 = await FirebaseFirestore.instance.collection('events').doc('hfW0ZCKZPstiHtuqIpje').get();
+                Event eventObj = Event.fromJson(eventInfo1.data()!);
+                events.add(eventObj);
+
+                final eventInfo2 = await FirebaseFirestore.instance.collection('events').doc('qkX3TETwezPjtdGw4esJ').get();
+                eventObj = Event.fromJson(eventInfo2.data()!);
+                events.add(eventObj);
+
+                await Navigator.of(context).pushNamed('/toPlanConfirmPage', arguments: events);
+                events.clear();
+              },
+              child: Card(
+                margin: EdgeInsets.all(10.0),
+                child: ListTile(
+                  title: Text(
+                      "Most Liked / Viewed Tour Now ;)",
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontFamily: "GmarketSansTTF",
+                        fontSize: 14,
+                      )
+                  ),
+                  subtitle: Text(
+                      "12 : 10 ~",
+                      style: TextStyle(
+                        fontFamily: "GmarketSansTTF",
+                        fontSize: 12,
+                      )
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 40,),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("# How About Your Own Plan?",
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontFamily: "GmarketSansTTF",
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold
+                  ),),
+              ],
+            ),
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: () async {
+
+                final eventInfo1 = await FirebaseFirestore.instance.collection('events').doc('hfW0ZCKZPstiHtuqIpje').get();
+                Event eventObj = Event.fromJson(eventInfo1.data()!);
+                events.add(eventObj);
+
+                final eventInfo2 = await FirebaseFirestore.instance.collection('events').doc('3JvlNTsYRDu5diXNzBAj').get();
+                eventObj = Event.fromJson(eventInfo2.data()!);
+                events.add(eventObj);
+
+                await Navigator.of(context).pushNamed('/toPlanConfirmPage', arguments: events);
+                events.clear();
+              },
+              child: Card(
+                margin: EdgeInsets.all(10.0),
+                child: ListTile(
+                  title: Text(
+                      "Food Tour :)",
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontFamily: "GmarketSansTTF",
+                        fontSize: 14,
+                      )
+                  ),
+                  subtitle: Text(
+                      "12 : 10 ~",
+                      style: TextStyle(
+                        fontFamily: "GmarketSansTTF",
+                        fontSize: 12,
+                      )
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
